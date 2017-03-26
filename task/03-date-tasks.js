f06a115ae7913ea4d5c6650a8ea59299ba9ddc24
kruglikovAlex <kruglikov_alex@tut.by>
2017-03-27 01:37:57 +0300

    Update the links

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,9 +22,9 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-    let newDate = new Date(value);
+    /*let newDate = new Date(value);
     let stringUTCDate = newDate.toUTCString();
-
+*/
     return Date.parse(value);
 }
 
